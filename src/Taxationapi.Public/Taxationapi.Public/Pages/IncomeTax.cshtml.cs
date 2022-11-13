7cd138b474e25fceede8ca00d35f5da4b77a2f84
Lars Holdgaard <mcoroklo@gmail.com>
2022-11-13 21:40:59 +0400

    Nice uI

---
+++ src/Taxationapi.Public/Taxationapi.Public/Pages/IncomeTax.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Taxationapi.Public.Pages
+{
+    public class IncomeTaxModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
